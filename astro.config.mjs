09a00cea24d5b394143f64f349d43bdc23175a7d
Yonathan Gashu <yonathangashu@gmail.com>
2026-01-22 22:11:52 -0500

    Add fonts and visual fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,20 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	experimental: {
+		fonts: [
+			{
+				name: "Charter",
+				cssVariable: "--font-charter",
+				provider: "local",
+				variants: [
+					{ src: ["./src/assets/fonts/Charter.woff2"] },
+					{ src: ["./src/assets/fonts/Charter-Bold.woff2"] },
+					{ src: ["./src/assets/fonts/Charter-Italic.woff2"] },
+					{ src: ["./src/assets/fonts/Charter-BoldItalic.woff2"] },
+				],
+			},
+		],
+	},
+});
