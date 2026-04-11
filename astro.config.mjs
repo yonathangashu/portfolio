1d392eba6f4df3701dead6e75b85817762072395
Yonathan Gashu <yonathangashu@gmail.com>
2026-04-11 12:01:14 -0400

    Fix linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ygashu.dev',
+	site: "https://ygashu.dev",
 	experimental: {
 		fonts: [
 			{
